c4c5d94d5d25cc93a6abb92c1c7e1b565accea7b
zaki <mzaki3969@gmail.com>
2025-06-19 15:03:54 +0700

    first commit

---
+++ app/Models/Dosen.php
@@ -0,0 +1,19 @@
+<?php   
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dosen extends Model{
+    protected $table = "dosens";
+    protected $primaryKey = 'no';
+    public $incrementing = true;
+    protected $fillable = [
+        'nip',
+        'nama_lengkap',
+        'no_telepon',
+        'email',
+        'alamat'
+    ];
+
+    // public $timestamps = false;
+}
